aeda7eddd132d679d1a4032dde521c4d9e302855
Nathan Yang <yhq911119@gmail.com>
2021-08-02 14:35:39 +1000

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9b8c81a2976c6540866556375b5884c662c92175',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9b8c81a2976c6540866556375b5884c662c92175',
+            'dev_requirement' => false,
+        ),
+    ),
+);
